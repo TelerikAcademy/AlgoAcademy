ca4536115d4be78dd11f020589a32abb3e7445ca
Doncho Minkov <DonchoMinkov@gmail.com>
2015-04-22 17:24:32 +0300

    Small fixes to code

--- 2015-04-Training-for-NOI/Problem Solving Demos/shuffing_deck.cpp
+++ 2015-04-Training-for-NOI/Problem Solving Demos/shuffing_deck.cpp
@@ -3,15 +3,13 @@
 #include <string>
 using namespace std;
 
-struct Card{
-	string face;
-	string suit;
+struct Card {
+  string face;
+  string suit;
 
-	Card();
-	Card(string, string);
+  Card();
+  Card(string, string);
 };
-
-// ostream& operator<<(std::ostream&, const Card&);
 
 void print(vector<Card>);
 
@@ -24,111 +22,103 @@ void testShuffleDeck1Card();
 void testShuffleDeck52Cards();
 void testShuffleDeck52000Cards();
 
-int main(){
-	testShuffleDeckNoCards();
-	testShuffleDeck1Card();
-	testShuffleDeck52Cards();
-	testShuffleDeck52000Cards();
+int main() {
+  testShuffleDeckNoCards();
+  testShuffleDeck1Card();
+  testShuffleDeck52Cards();
+  testShuffleDeck52000Cards();
 
-	return 0;
+  return 0;
 }
 
-Card::Card(){
+Card::Card() {}
+
+Card::Card(string f, string s) {
+  this->face = f;
+  this->suit = s;
 }
 
-Card::Card(string f, string s){
-	this->face = f;
-	this->suit = s;
+void performSingleExchange(vector<Card> &deck, int index) {
+  static bool isSeedSet = false;
+  if (!isSeedSet) {
+    srand(time(NULL));
+    isSeedSet = true;
+  }
+  int randomIndex = 1 + (rand() % (deck.size() - 1));
+
+  Card first = deck[index];
+  Card randomCard = deck[randomIndex];
+  deck[index] = randomCard;
+  deck[randomIndex] = first;
 }
 
-void performSingleExchange(vector<Card> &deck, int index){
-	static bool isSeedSet = false;
-	if(!isSeedSet){
-		srand(time(NULL));
-		isSeedSet = true;
-	}
-	int randomIndex = 1 + (rand() % (deck.size() - 1));
-
-	Card first = deck[index];
-	Card randomCard = deck[randomIndex];
-	deck[index] = randomCard;
-	deck[randomIndex] = first;
+void shuffleDeck(vector<Card> &deck) {
+  if (deck.size() <= 1) {
+    return;
+  }
+  for (int i = 0, size = deck.size(); i < size; ++i) {
+    performSingleExchange(deck, i);
+  }
 }
 
-void shuffleDeck(vector<Card> & deck){
-	if(deck.size() <=1){
-		return;
-	}
-	for(int i = 0, size = deck.size(); i < size; ++i){
-		performSingleExchange(deck, i);
-	}
+void print(vector<Card> deck) {
+  static string line = "--------------------------------------";
+  cout << line << endl;
+  for (auto card : deck) {
+    cout << "(" << card.face << " of " << card.suit << ")";
+  }
+  cout << endl;
+  cout << line << endl;
 }
 
-void print(vector<Card> deck){
-	static string line = "--------------------------------------";
-	cout<<line<<endl;
-	for(auto card : deck){
-		cout << "(" << card.face << " of "<< card.suit << ")";
-	}
-	cout<<endl;
-	cout<<line<<endl;
+void testShuffleDeckNoCards() {
+  vector<Card> deck;
+  print(deck);
+  shuffleDeck(deck);
+  print(deck);
 }
 
-void testShuffleDeckNoCards(){
-	vector<Card> deck;
-	print(deck);
-	shuffleDeck(deck);
-	print(deck);
+void testShuffleDeck1Card() {
+  vector<Card> deck;
+  deck.push_back(Card("A", "Spaces"));
+  print(deck);
+  shuffleDeck(deck);
+  print(deck);
 }
 
-void testShuffleDeck1Card(){
-	vector<Card> deck;
-	deck.push_back(Card("A", "Spaces"));
-	print(deck);
-	shuffleDeck(deck);
-	print(deck);
+void testShuffleDeck52Cards() {
+  string faces[] = {"2", "3",  "4", "5", "6", "7", "8",
+                    "9", "10", "J", "Q", "K", "A"};
+  string suits[] = {"Spades", "Hearts", "Diamonds", "Clubs"};
+
+  vector<Card> deck;
+
+  for (string face : faces) {
+    for (string suit : suits) {
+      deck.push_back(Card(face, suit));
+    }
+  }
+  print(deck);
+
+  shuffleDeck(deck);
+  print(deck);
 }
 
-void testShuffleDeck52Cards(){
-	string faces[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-	string suits[] = {"Spades", "Hearts", "Diamonds", "Clubs" };
-	int facesSize = sizeof(faces) / sizeof(string);
-	int suitsSize = sizeof(suits) / sizeof(string);
+void testShuffleDeck52000Cards() {
+  string faces[] = {"2", "3",  "4", "5", "6", "7", "8",
+                    "9", "10", "J", "Q", "K", "A"};
+  string suits[] = {"Spades", "Hearts", "Diamonds", "Clubs"};
+  vector<Card> deck;
 
-	vector<Card> deck;
+  for (int i = 0; i < 1000; i++) {
+    for (string face : faces) {
+      for (string suit : suits) {
+        deck.push_back(Card(face, suit));
+      }
+    }
+  }
+  print(deck);
 
-	for(string face:faces){
-		for(string suit:suits){
-			deck.push_back(Card(face, suit));
-		}
-	}
-	print(deck);
-
-	shuffleDeck(deck);
-	print(deck);
+  shuffleDeck(deck);
+  print(deck);
 }
-
-
-void testShuffleDeck52000Cards(){
-	string faces[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-	string suits[] = {"Spades", "Hearts", "Diamonds", "Clubs" };
-	vector<Card> deck;
-
-	for(int i = 0; i < 1000; i++){
-		for(string face:faces){
-			for(string suit:suits){
-				deck.push_back(Card(face, suit));
-			}
-		}
-	}
-	print(deck);
-
-	shuffleDeck(deck);
-	print(deck);
-}
-
-
-
-
-
-
