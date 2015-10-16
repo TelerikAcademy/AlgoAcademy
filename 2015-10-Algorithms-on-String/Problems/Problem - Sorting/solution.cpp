fbe2188aacf628ae61a9d96ac05f5b7a27f54d43
Doncho Minkov <donchominkov@gmail.com>
2015-10-16 16:16:11 +0300

    Renamed sorting.md to README.md, refactored solution

--- 2015-10-Algorithms-on-String/Problems/Problem - Sorting/solution.cpp
+++ 2015-10-Algorithms-on-String/Problems/Problem - Sorting/solution.cpp
@@ -1,29 +1,31 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#define SYMBOLS_COUNT 256
 
-int A[256];
 int main()
 {
-	int n;
-	cin>>n;
-	for(int i=0;i<n;++i)
-	{
-		int c;
-		cin>>c;
-		++A[c];
-	}
-	A[(int)'x'] = 0;
-	for(int i=0; i<26; ++i)
-	{
-		for(int j=0; j<A['A'+i]; ++j)
-			cout<<(char)('A'+i);
-		for(int j=0; j<A['a'+i]; ++j)
-			cout<<(char)('a'+i);
-	}
-	for(int i=1; i<10; ++i)
-		for(int j=0; j<A['0'+i]; ++j)
-			cout<<(char)('0'+i);
-	for(int j=0; j<A[(int)'0']; ++j)
-		cout<<'0';
-	cout<<'\n';
+    int counts[SYMBOLS_COUNT];
+    int n;
+
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+	int c;
+	std::cin >> c;
+	++counts[c];
+    }
+    counts[(int)'x'] = 0;
+
+    for (int i = 0; i < 26; ++i) {
+	for (int j = 0; j < counts['A' + i]; ++j)
+	    std::cout << (char)('A' + i);
+	for (int j = 0; j < counts['a' + i]; ++j)
+	    std::cout << (char)('a' + i);
+    }
+
+    for (int i = 1; i < 10; ++i)
+	for (int j = 0; j < counts['0' + i]; ++j)
+	    std::cout << (char)('0' + i);
+    for (int j = 0; j < counts[(int)'0']; ++j)
+	std::cout << '0';
+    std::cout << '\n';
 }
